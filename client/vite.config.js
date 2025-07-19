8ca5110e1c6c410beae1e6996bdb6cfe914ffde2
PRAGYA SONARE <163843602+pragyasonare@users.noreply.github.com>
2025-07-19 19:59:57 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000' // backend port
+      '/api': 'https://moodlog.onrender.com' // backend port
     }
   }
 })
