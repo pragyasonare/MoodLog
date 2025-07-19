ad0c61a0bd4798639ceb20f853da0fadb5b9a637
PRAGYA SONARE <163843602+pragyasonare@users.noreply.github.com>
2025-07-19 21:08:32 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,3 +1,10 @@
+// const api = axios.create({
+//   baseURL: 'http://localhost:3000/api', // Your backend port
+// });
+
+
+
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Your backend port
+  baseURL: 'https://moodlog.onrender.com',
+  withCredentials: true
 });
